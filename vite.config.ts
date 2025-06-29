1c075acb92cc00bfba5886ed0769e8f891653941
Andrew <andrewmuchangi254@gmail.com>
2025-06-29 15:59:24 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ["d399-102-0-8-22.ngrok-free.app"],
+		allowedHosts: ["a9a9-102-0-8-22.ngrok-free.app"],
 		host: true, // Allow access from any host
 		port: 5173, // Default Vite port
 	}
